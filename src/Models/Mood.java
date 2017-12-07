71fea038c0b086ebf09080b822bf2f86d66af661
Walrus <Walrus@Walrus-PC>
2017-12-07 15:37:18 -0600

    added strength and type fields to Mood

--- src/Models/Mood.java
+++ src/Models/Mood.java
@@ -9,16 +9,34 @@ import java.time.LocalDateTime;
 
 public class Mood extends Entry{
 
+
     /**
      * Constructor that initializes attributes
      * @param date is the date and time of the entry
      * @param name is the name of the food item as a String
      */
-    public Mood(LocalDateTime date, String name) {
+    
+    private final String type = "Mood";
+    private int strength;
+    
+    public Mood(LocalDateTime date, String name, int strength) {
         super(date, name);
+        this.strength = strength;
     }
     
     public String toString(){
         return getName();
+    }
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+    /**
+     * @return the strength
+     */
+    public int getStrength() {
+        return strength;
     }
 }
